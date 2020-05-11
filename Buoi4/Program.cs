574834aa65b026c93690d2b6bd8a8a1a8469397b
S2Wind <50346687+S2Wind@users.noreply.github.com>
2020-05-12 00:10:37 +0700

    Buoi 4

---
+++ Buoi4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LopHoc lop = new LopHoc(5);
+            lop.Nhap(5);
+
+            lop.ThanhToanCongNo("Phan Dac Son",1000000);
+            lop.NgauNhienCrush();
+            lop.CoNguoiYeuLaMatTien();
+
+            Console.ReadKey();
+
+        }
+    }
+}
